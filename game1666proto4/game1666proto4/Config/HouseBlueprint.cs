0e0c7df0aa2843fd1bc49302512d593ec1f7b1bb
sgolodetz <sgolodetz@gxstudios.net>
2011-11-16 23:16:42 +0000

    Started fleshing out house blueprints.

--- game1666proto4/game1666proto4/Config/HouseBlueprint.cs
+++ game1666proto4/game1666proto4/Config/HouseBlueprint.cs
@@ -10,7 +10,22 @@ namespace game1666proto4
 		//#################### PUBLIC METHODS ####################
 		#region
 
+		/// <summary>
+		/// Constructs a house blueprint from its XML representation.
+		/// </summary>
+		/// <param name="blueprintXml">The XML representation of the blueprint.</param>
+		/// <returns>The house blueprint.</returns>
 		public static HouseBlueprint LoadFromXml(string blueprintXml)
+		{
+			// TODO
+			throw new System.NotImplementedException();
+		}
+
+		/// <summary>
+		/// Returns the maximum number of people that can occupy this house.
+		/// </summary>
+		/// <returns>As specified above.</returns>
+		public int MaxOccupants()
 		{
 			// TODO
 			throw new System.NotImplementedException();
