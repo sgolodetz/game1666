1dbb9ee8b93bf2fcbd7c4e42e9010708b786226f
sgolodetz <sgolodetz@gxstudios.net>
2011-11-16 23:20:50 +0000

    Tidied up Game.cs using code from the previous prototype.

--- game1666proto4/game1666proto4/Game.cs
+++ game1666proto4/game1666proto4/Game.cs
@@ -9,53 +9,103 @@ using Microsoft.Xna.Framework.Input;
 
 namespace game1666proto4
 {
-	/// <summary>
-	/// This is the main type for your game
-	/// </summary>
 	sealed class Game : Microsoft.Xna.Framework.Game
 	{
-		private readonly GraphicsDeviceManager m_graphics;
-		private SpriteBatch m_spriteBatch;
+		//#################### PRIVATE VARIABLES ####################
+		#region
 
+		private BasicEffect m_basicEffect;
+		private readonly GraphicsDeviceManager m_graphics;
+
+		#endregion
+
+		//#################### CONSTRUCTORS ####################
+		#region
+
+		/// <summary>
+		/// Sets up the graphics device and window, and sets the root directory for content.
+		/// </summary>
 		public Game()
 		{
 			m_graphics = new GraphicsDeviceManager(this);
+			if(GraphicsAdapter.DefaultAdapter.IsProfileSupported(GraphicsProfile.HiDef))
+			{
+				m_graphics.GraphicsProfile = GraphicsProfile.HiDef;
+			}
+			m_graphics.PreferredBackBufferWidth = 640;
+			m_graphics.PreferredBackBufferHeight = 480;
+
+			this.IsMouseVisible = true;
+
 			Content.RootDirectory = "Content";
+		}
+
+		#endregion
+
+		//#################### PROTECTED METHODS ####################
+		#region
+
+		/// <summary>
+		/// Called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// Set up the view matrix.
+			m_basicEffect.View = Matrix.CreateLookAt(new Vector3(10, -20, 20), new Vector3(5, 20, 0), new Vector3(0, 0, 1));
+
+			// Set up the world matrix.
+			m_basicEffect.World = Matrix.Identity;
+
+			// Set up the rasterizer state.
+			var rasterizerState = new RasterizerState();
+			rasterizerState.CullMode = CullMode.CullClockwiseFace;
+			GraphicsDevice.RasterizerState = rasterizerState;
+
+			// TODO
+
+			base.Draw(gameTime);
 		}
 
 		/// <summary>
 		/// Allows the game to perform any initialization it needs to before starting to run.
 		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
+		/// related content. Calling base.Initialize will enumerate through any components
 		/// and initialize them as well.
 		/// </summary>
 		protected override void Initialize()
 		{
-			// TODO: Add your initialization logic here
+			// Load the game configuration from XML.
 			GameConfig.Load(@"Content\GameConfig.xml");
+
+			// Set up the basic effect.
+			m_basicEffect = new BasicEffect(GraphicsDevice);
+			
+			// Set up the projection matrix.
+			m_basicEffect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), (float)m_graphics.PreferredBackBufferWidth / m_graphics.PreferredBackBufferHeight, 0.1f, 1000.0f);
+
+			// TODO
 
 			base.Initialize();
 		}
 
 		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
+		/// Called once per game to allow content to be loaded.
 		/// </summary>
 		protected override void LoadContent()
 		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			m_spriteBatch = new SpriteBatch(GraphicsDevice);
-
-			// TODO: use this.Content to load your game content here
+			// Pre-load content.
+			// TODO
 		}
 
 		/// <summary>
-		/// UnloadContent will be called once per game and is the place to unload
-		/// all content.
+		/// Called once per game to allow content to be unloaded.
 		/// </summary>
 		protected override void UnloadContent()
 		{
-			// TODO: Unload any non ContentManager content here
+			Content.Dispose();
 		}
 
 		/// <summary>
@@ -65,26 +115,16 @@ namespace game1666proto4
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
-			// Allows the game to exit
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-				this.Exit();
+			if(Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
+				Exit();
+			}
 
-			// TODO: Add your update logic here
+			// TODO
 
 			base.Update(gameTime);
 		}
 
-		/// <summary>
-		/// This is called when the game should draw itself.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
-		protected override void Draw(GameTime gameTime)
-		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
-
-			// TODO: Add your drawing code here
-
-			base.Draw(gameTime);
-		}
+		#endregion
 	}
 }
