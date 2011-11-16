94c6b851dd1db59ac0d5c3b086b4debe7560ff1f
sgolodetz <sgolodetz@gxstudios.net>
2011-11-16 23:23:37 +0000

    Changed HouseBlueprint.MaxOccupants into a property.

--- game1666proto4/game1666proto4/Config/HouseBlueprint.cs
+++ game1666proto4/game1666proto4/Config/HouseBlueprint.cs
@@ -7,6 +7,23 @@ namespace game1666proto4
 {
 	sealed class HouseBlueprint : Blueprint
 	{
+		//#################### PROPERTIES ####################
+		#region
+
+		/// <summary>
+		/// The maximum number of people that can occupy this house.
+		/// </summary>
+		public int MaxOccupants
+		{
+			get
+			{
+				// TODO
+				throw new System.NotImplementedException();
+			}
+		}
+
+		#endregion
+
 		//#################### PUBLIC METHODS ####################
 		#region
 
@@ -16,16 +33,6 @@ namespace game1666proto4
 		/// <param name="blueprintXml">The XML representation of the blueprint.</param>
 		/// <returns>The house blueprint.</returns>
 		public static HouseBlueprint LoadFromXml(string blueprintXml)
-		{
-			// TODO
-			throw new System.NotImplementedException();
-		}
-
-		/// <summary>
-		/// Returns the maximum number of people that can occupy this house.
-		/// </summary>
-		/// <returns>As specified above.</returns>
-		public int MaxOccupants()
 		{
 			// TODO
 			throw new System.NotImplementedException();
