b2ceba2cef00edf1b4372a70b4d4ecc9a33465f1
sgolodetz <sgolodetz@gxstudios.net>
2011-11-21 23:38:01 +0000

    Sorted out bounding boxes for quadtree nodes.

--- game1666proto4/game1666proto4/Model/Terrain/QuadtreeNode.cs
+++ game1666proto4/game1666proto4/Model/Terrain/QuadtreeNode.cs
@@ -17,9 +17,9 @@ namespace game1666proto4
 		//#################### PRIVATE VARIABLES ####################
 		#region
 
-		private readonly BoundingBox m_bounds;							/// an AABB that bounds the terrain segment represented by this node
-		private readonly QuadtreeNode[] m_children;						/// the children of this node in the quadtree (if any)
-		private readonly IDictionary<Vector2i,Triangle[]> m_triangles;	/// the triangles in the terrain segment represented by this node
+		private readonly BoundingBox m_bounds;								/// an AABB that bounds the terrain segment represented by this node
+		private readonly QuadtreeNode[] m_children;							/// the children of this node in the quadtree (if any)
+		private readonly IDictionary<Vector2i,Triangle[]> m_triangleMap;	/// the triangles in the terrain segment represented by this node
 
 		#endregion
 
@@ -33,17 +33,49 @@ namespace game1666proto4
 		public QuadtreeNode(QuadtreeNode[] children)
 		{
 			m_children = children;
-			// TODO: Calculate bounding box.
+
+			// Calculate the tightest axis-aligned bounding box that surrounds all the children of this quadtree node.
+			var mins = new Vector3(float.MaxValue);
+			var maxs = new Vector3(float.MinValue);
+			foreach(QuadtreeNode child in children)
+			{
+				mins.X = Math.Min(mins.X, child.m_bounds.Min.X);
+				mins.Y = Math.Min(mins.Y, child.m_bounds.Min.Y);
+				mins.Z = Math.Min(mins.Z, child.m_bounds.Min.Z);
+				maxs.X = Math.Max(maxs.X, child.m_bounds.Max.X);
+				maxs.Y = Math.Max(maxs.Y, child.m_bounds.Max.Y);
+				maxs.Z = Math.Max(maxs.Z, child.m_bounds.Max.Z);
+			}
+			m_bounds = new BoundingBox(mins, maxs);
 		}
 
 		/// <summary>
 		/// Constructs a (leaf) quadtree node to contain a set of triangles.
 		/// </summary>
-		/// <param name="triangles">The triangles, grouped by grid square.</param>
-		public QuadtreeNode(IDictionary<Vector2i,Triangle[]> triangles)
+		/// <param name="triangleMap">The triangles, grouped by grid square.</param>
+		public QuadtreeNode(IDictionary<Vector2i,Triangle[]> triangleMap)
 		{
-			m_triangles = triangles;
-			// TODO: Calculate bounding box.
+			m_triangleMap = triangleMap;
+
+			// Calculate the tightest axis-aligned bounding box that surrounds all the triangles in this quadtree node.
+			var mins = new Vector3(float.MaxValue);
+			var maxs = new Vector3(float.MinValue);
+			foreach(KeyValuePair<Vector2i,Triangle[]> gridSquare in triangleMap)
+			{
+				foreach(Triangle triangle in gridSquare.Value)
+				{
+					foreach(Vector3 v in triangle.Vertices)
+					{
+						mins.X = Math.Min(mins.X, v.X);
+						mins.Y = Math.Min(mins.Y, v.Y);
+						mins.Z = Math.Min(mins.Z, v.Z);
+						maxs.X = Math.Max(maxs.X, v.X);
+						maxs.Y = Math.Max(maxs.Y, v.Y);
+						maxs.Z = Math.Max(maxs.Z, v.Z);
+					}
+				}
+			}
+			m_bounds = new BoundingBox(mins, maxs);
 		}
 
 		#endregion
