55b5fcb0ab0dd00060a91f3348dfe101989fcd76
sgolodetz <sgolodetz@gxstudios.net>
2012-01-03 22:33:21 +0000

    Started writing tests for game1666proto4 using xUnit.

---
+++ game1666proto4Test/Common/Maths/Vector2iTest.cs
@@ -0,0 +1,18 @@
+﻿using game1666proto4.Common.Maths;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Xunit.Assert;
+
+namespace game1666proto4Test.Common.Maths
+{
+	[TestClass]
+	public class Vector2iTest
+	{
+		[TestMethod]
+		public void op_AdditionTest()
+		{
+			Vector2i v1 = new Vector2i(2, 3);
+			Vector2i v2 = new Vector2i(5, 7);
+			Assert.Equal(v1 + v2, new Vector2i(7, 10));
+		}
+	}
+}
--- game1666proto4Test/Properties/AssemblyInfo.cs
+++ game1666proto4Test/Properties/AssemblyInfo.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("game1666proto4")]
-[assembly: AssemblyProduct("game1666proto4")]
+[assembly: AssemblyTitle("game1666proto4Test")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyProduct("game1666proto4Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("300b0dab-6b4b-4d65-b245-2ef6961ed8b8")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("53799b56-f8ce-44fe-9f79-88d36a048b94")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,10 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
-
-// Mark the assembly as compliant to the Common Language Specification.
-[assembly: CLSCompliant(true)]
-
-// Make the assembly's internals visible to its test project.
-[assembly:InternalsVisibleTo("game1666proto4Test")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
