84f1eb78724b3b6659a15a29dd23068b4d6723f1
sgolodetz <sgolodetz@gxstudios.net>
2012-01-13 00:18:38 +0000

    Moved ToolUtil into the right namespace.

--- game1666proto4/game1666proto4/UI/Tools/ToolUtil.cs
+++ game1666proto4/game1666proto4/UI/Tools/ToolUtil.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace game1666proto4
+namespace game1666proto4.UI.Tools
 {
 	/// <summary>
 	/// This class provides utility methods that can be used by the various tools.
