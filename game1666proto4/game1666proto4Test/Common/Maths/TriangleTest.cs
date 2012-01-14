cda82a1ffe01046007e185851328ce38113d5c8b
sgolodetz <sgolodetz@gxstudios.net>
2012-01-14 16:24:56 +0000

    Added some more unit tests for Triangle and sorted out a build configuration problem.

--- game1666proto4/game1666proto4Test/Common/Maths/TriangleTest.cs
+++ game1666proto4/game1666proto4Test/Common/Maths/TriangleTest.cs
@@ -3,9 +3,10 @@
  * Copyright 2012. All rights reserved.
  ***/
 
+using System;
 using game1666proto4.Common.Maths;
-using Microsoft.Xna.Framework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
 using Assert = Xunit.Assert;
 
 namespace game1666proto4Test.Common.Maths
@@ -16,7 +17,7 @@ namespace game1666proto4Test.Common.Maths
 		[TestMethod]
 		public void ConstructorTest()
 		{
-			// A simple triangle in the z = 3 plane
+			// A triangle in the z = 3 plane
 			{
 			var v0 = new Vector3(1,2,3);
 			var v1 = new Vector3(2,2,3);
@@ -26,6 +27,24 @@ namespace game1666proto4Test.Common.Maths
 			Assert.Equal(triangle.Vertices[1], v1);
 			Assert.Equal(triangle.Vertices[2], v2);
 			Assert.Equal(triangle.Normal, Vector3.UnitZ);
+			}
+
+			// A triangle in the x + y = 2 plane
+			{
+				var v0 = new Vector3(2, 0, 0);
+				var v1 = new Vector3(0, 2, 0);
+				var v2 = new Vector3(2, 0, 2);
+				var triangle = new Triangle(v0, v1, v2);
+				var desiredNormal = new Vector3(1, 1, 0);
+				desiredNormal.Normalize();
+				Assert.Equal(triangle.Normal, desiredNormal);
+			}
+
+			// A degenerate triangle
+			{
+				var v0 = new Vector3(1, 0, 0);
+				var v1 = new Vector3(0, 1, 0);
+				Assert.Throws<InvalidOperationException>(() => new Triangle(v0, v1, v0));
 			}
 		}
 	}
