429ca6e2bd12d2cc962c2a824248083d05b6adb3
sgolodetz <sgolodetz@gxstudios.net>
2012-01-14 16:34:25 +0000

    Added a test for Triangle.DeterminePlane.

--- game1666proto4/game1666proto4Test/Common/Maths/TriangleTest.cs
+++ game1666proto4/game1666proto4Test/Common/Maths/TriangleTest.cs
@@ -19,14 +19,14 @@ namespace game1666proto4Test.Common.Maths
 		{
 			// A triangle in the z = 3 plane
 			{
-			var v0 = new Vector3(1,2,3);
-			var v1 = new Vector3(2,2,3);
-			var v2 = new Vector3(2,3,3);
-			var triangle = new Triangle(v0, v1, v2);
-			Assert.Equal(triangle.Vertices[0], v0);
-			Assert.Equal(triangle.Vertices[1], v1);
-			Assert.Equal(triangle.Vertices[2], v2);
-			Assert.Equal(triangle.Normal, Vector3.UnitZ);
+				var v0 = new Vector3(1,2,3);
+				var v1 = new Vector3(2,2,3);
+				var v2 = new Vector3(2,3,3);
+				var triangle = new Triangle(v0, v1, v2);
+				Assert.Equal(triangle.Vertices[0], v0);
+				Assert.Equal(triangle.Vertices[1], v1);
+				Assert.Equal(triangle.Vertices[2], v2);
+				Assert.Equal(triangle.Normal, Vector3.UnitZ);
 			}
 
 			// A triangle in the x + y = 2 plane
@@ -45,6 +45,22 @@ namespace game1666proto4Test.Common.Maths
 				var v0 = new Vector3(1, 0, 0);
 				var v1 = new Vector3(0, 1, 0);
 				Assert.Throws<InvalidOperationException>(() => new Triangle(v0, v1, v0));
+			}
+		}
+
+		[TestMethod]
+		public void DeterminePlaneTest()
+		{
+			// A triangle in the 4x - 3y - 12 = 0 plane
+			{
+				var v0 = new Vector3(0, -4, 0);
+				var v1 = new Vector3(3, 0, 0);
+				var v2 = new Vector3(0, -4, 23);
+				Plane plane = new Triangle(v0, v1, v2).DeterminePlane();
+				var desiredNormal = new Vector3(4, -3, 0);
+				Assert.Equal(plane.D, -12 / desiredNormal.Length());
+				desiredNormal.Normalize();
+				Assert.Equal(plane.Normal, desiredNormal);
 			}
 		}
 	}
