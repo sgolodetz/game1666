96baeafc053f22ecc718a8a6fc8ec5ce06539ebd
sgolodetz <sgolodetz@gxstudios.net>
2012-02-29 21:43:44 +0000

    Split the two tests for MatchmakingGraph.FindBestMatching into separate test methods.

--- game1666proto4/game1666proto4Test/Common/Matchmaking/MatchmakingGraphTest.cs
+++ game1666proto4/game1666proto4Test/Common/Matchmaking/MatchmakingGraphTest.cs
@@ -15,48 +15,48 @@ namespace game1666proto4Test.Common.Matchmaking
 	public sealed class MatchmakingGraphTest
 	{
 		[TestMethod]
-		public void FindBestMatchingTest()
+		public void FindBestMatchingTest_3()
 		{
-			{
-				var graph = new MatchmakingGraph(3, 3);
-				graph.AddEdge(0, 0, 5);
-				graph.AddEdge(0, 1, 5);
-				graph.AddEdge(0, 2, 4);
-				graph.AddEdge(1, 0, 4);
-				graph.AddEdge(1, 1, 6);
-				graph.AddEdge(2, 1, 7);
-				graph.AddEdge(2, 2, 3);
+			var graph = new MatchmakingGraph(3, 3);
+			graph.AddEdge(0, 0, 5);
+			graph.AddEdge(0, 1, 5);
+			graph.AddEdge(0, 2, 4);
+			graph.AddEdge(1, 0, 4);
+			graph.AddEdge(1, 1, 6);
+			graph.AddEdge(2, 1, 7);
+			graph.AddEdge(2, 2, 3);
 
-				graph.FindBestMatching();
+			graph.FindBestMatching();
 
-				List<MatchmakingEdge> matchingEdges = graph.MatchingEdges.ToList();
-				Assert.True(matchingEdges.Count == 3);
-				Assert.True(matchingEdges.Contains(new MatchmakingEdge(0, 2, 4)));
-				Assert.True(matchingEdges.Contains(new MatchmakingEdge(1, 0, 4)));
-				Assert.True(matchingEdges.Contains(new MatchmakingEdge(2, 1, 7)));
-			}
+			List<MatchmakingEdge> matchingEdges = graph.MatchingEdges.ToList();
+			Assert.True(matchingEdges.Count == 3);
+			Assert.True(matchingEdges.Contains(new MatchmakingEdge(0, 2, 4)));
+			Assert.True(matchingEdges.Contains(new MatchmakingEdge(1, 0, 4)));
+			Assert.True(matchingEdges.Contains(new MatchmakingEdge(2, 1, 7)));
+		}
 
-			{
-				var graph = new MatchmakingGraph(5, 5);
-				graph.AddEdge(0, 1, 1);
-				graph.AddEdge(1, 0, 4);
-				graph.AddEdge(1, 2, 3);
-				graph.AddEdge(2, 1, 10);
-				graph.AddEdge(2, 3, 9);
-				graph.AddEdge(3, 2, 6);
-				graph.AddEdge(3, 3, 4);
-				graph.AddEdge(3, 4, 5);
-				graph.AddEdge(4, 3, 3);
+		[TestMethod]
+		public void FindBestMatchingTest_5()
+		{
+			var graph = new MatchmakingGraph(5, 5);
+			graph.AddEdge(0, 1, 1);
+			graph.AddEdge(1, 0, 4);
+			graph.AddEdge(1, 2, 3);
+			graph.AddEdge(2, 1, 10);
+			graph.AddEdge(2, 3, 9);
+			graph.AddEdge(3, 2, 6);
+			graph.AddEdge(3, 3, 4);
+			graph.AddEdge(3, 4, 5);
+			graph.AddEdge(4, 3, 3);
 
-				graph.FindBestMatching();
+			graph.FindBestMatching();
 
-				List<MatchmakingEdge> matchingEdges = graph.MatchingEdges.ToList();
-				Assert.True(matchingEdges.Count == 4);
-				Assert.True(matchingEdges.Contains(new MatchmakingEdge(1, 0, 4)));
-				Assert.True(matchingEdges.Contains(new MatchmakingEdge(2, 1, 10)));
-				Assert.True(matchingEdges.Contains(new MatchmakingEdge(3, 2, 6)));
-				Assert.True(matchingEdges.Contains(new MatchmakingEdge(4, 3, 3)));
-			}
+			List<MatchmakingEdge> matchingEdges = graph.MatchingEdges.ToList();
+			Assert.True(matchingEdges.Count == 4);
+			Assert.True(matchingEdges.Contains(new MatchmakingEdge(1, 0, 4)));
+			Assert.True(matchingEdges.Contains(new MatchmakingEdge(2, 1, 10)));
+			Assert.True(matchingEdges.Contains(new MatchmakingEdge(3, 2, 6)));
+			Assert.True(matchingEdges.Contains(new MatchmakingEdge(4, 3, 3)));
 		}
 	}
 }
