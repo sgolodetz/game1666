b771710980f8c5daed3cbd2e72f9b1b0ade3ba04
sgolodetz <sgolodetz@gxstudios.net>
2012-04-07 23:09:29 +0100

    Started work on PlayingAreaComponent and EntityFactory.

---
+++ game1666/game1666/GameModel/Entities/Components/Internal/PlayingAreaComponent.cs
@@ -0,0 +1,60 @@
+﻿/***
+ * game1666: PlayingAreaComponent.cs
+ * Copyright Stuart Golodetz, 2012. All rights reserved.
+ ***/
+
+using System.Collections.Generic;
+using System.Xml.Linq;
+using game1666.Common.Entities;
+
+namespace game1666.GameModel.Entities.Components.Internal
+{
+	/// <summary>
+	/// An instance of this class provides playing area behaviour to an entity such
+	/// as the world or a settlement. Playing areas have a terrain on which other
+	/// entities can be placed or move around.
+	/// </summary>
+	sealed class PlayingAreaComponent : EntityComponent
+	{
+		//#################### PROPERTIES ####################
+		#region
+
+		/// <summary>
+		/// The group of the component.
+		/// </summary>
+		public override string Group { get { return StaticGroup; } }
+
+		/// <summary>
+		/// The name of the component.
+		/// </summary>
+		public override string Name { get { return "PlayingArea"; } }
+
+		/// <summary>
+		/// The group of the component.
+		/// </summary>
+		public static string StaticGroup { get { return "GameModel/Internal"; } }
+
+		#endregion
+
+		//#################### CONSTRUCTORS ####################
+		#region
+
+		/// <summary>
+		/// Constructs a playing area component directly from its properties.
+		/// </summary>
+		/// <param name="properties">The properties of the component.</param>
+		public PlayingAreaComponent(IDictionary<string,dynamic> properties)
+		:	base(properties)
+		{}
+
+		/// <summary>
+		/// Constructs a playing area component from its XML representation.
+		/// </summary>
+		/// <param name="componentElt">The root element of the component's XML representation.</param>
+		public PlayingAreaComponent(XElement componentElt)
+		:	base(componentElt)
+		{}
+
+		#endregion
+	}
+}
---
+++ game1666/game1666/GameModel/Entities/EntityFactory.cs
@@ -0,0 +1,32 @@
+﻿/***
+ * game1666: EntityFactory.cs
+ * Copyright Stuart Golodetz, 2012. All rights reserved.
+ ***/
+
+using System.Collections.Generic;
+using game1666.Common.Entities;
+using game1666.GameModel.Entities.Components.Internal;
+
+namespace game1666.GameModel.Entities
+{
+	/// <summary>
+	/// This class constructs entities that form part of the game model.
+	/// </summary>
+	static class EntityFactory
+	{
+		/// <summary>
+		/// Makes a new world entity.
+		/// </summary>
+		/// <returns>The constructed entity.</returns>
+		public static Entity MakeWorld(/* TODO: Terrain */)
+		{
+			var world = new Entity(".", "World");
+
+			new PlayingAreaComponent(new Dictionary<string,dynamic>
+			{
+			}).AddToEntity(world);
+
+			return world;
+		}
+	}
+}
