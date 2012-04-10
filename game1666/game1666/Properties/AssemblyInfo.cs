fe432f09e24494887864bb68317fa4b4b769a627
sgolodetz <sgolodetz@gxstudios.net>
2012-04-10 20:24:56 +0100

    Made a couple of minor changes suggested by NDepend.

--- game1666/game1666/Properties/AssemblyInfo.cs
+++ game1666/game1666/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -33,4 +33,8 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 
+// Mark the assembly as compliant to the Common Language Specification.
+[assembly: CLSCompliant(true)]
+
+// Make the assembly's internals visible to its test project.
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("game1666Test")]
