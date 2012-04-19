f4411aa186c54262961535b304f07fdfdd823d58
sgolodetz <sgolodetz@gxstudios.net>
2012-04-19 19:43:00 +0100

    Started implementing the finite state machine for PlaceableComponent.

--- game1666/game1666/GameModel/Entities/Components/External/PlacementComponent.cs
+++ game1666/game1666/GameModel/Entities/Components/External/PlacementComponent.cs
@@ -3,14 +3,26 @@
  * Copyright Stuart Golodetz, 2012. All rights reserved.
  ***/
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
 using game1666.Common.Maths;
 using game1666.GameModel.Blueprints;
+using Microsoft.Xna.Framework;
 
 namespace game1666.GameModel.Entities.Components.External
 {
+	/// <summary>
+	/// The various possible states of a placement component.
+	/// </summary>
+	enum PlacementComponentState
+	{
+		IN_CONSTRUCTION,	// the entity containing the component is in the process of being constructed
+		IN_DESTRUCTION,		// the entity containing the component is in the process of being destructed
+		OPERATING			// the entity containing the component is operating normally
+	}
+
 	/// <summary>
 	/// An instance of this class provides placement behaviour to an entity,
 	/// i.e. entities with this component can be placed on a terrain.
@@ -43,6 +55,15 @@ namespace game1666.GameModel.Entities.Components.External
 		/// The blueprint for the entity.
 		/// </summary>
 		public PlacementBlueprint Blueprint { get; private set; }
+
+		/// <summary>
+		/// The amount of construction done (in comparison to the time required to construct the entity).
+		/// </summary>
+		private int ConstructionDone
+		{
+			get { return Properties["ConstructionDone"]; }
+			set { Properties["ConstructionDone"] = Math.Max(Math.Min(value, Blueprint.TimeToConstruct), 0); }
+		}
 
 		/// <summary>
 		/// Whether or not the entity can be destroyed.
@@ -83,6 +104,15 @@ namespace game1666.GameModel.Entities.Components.External
 		/// </summary>
 		public Vector2i Position { get { return Properties["Position"]; } }
 
+		/// <summary>
+		/// The state of the component.
+		/// </summary>
+		public PlacementComponentState State
+		{
+			get { return Enum.Parse(typeof(PlacementComponentState), Properties["State"]); }
+			set { Properties["State"] = value.ToString(); }
+		}
+
 		#endregion
 
 		//#################### CONSTRUCTORS ####################
@@ -99,6 +129,45 @@ namespace game1666.GameModel.Entities.Components.External
 
 			// Determine the entity's (suitably rotated) footprint.
 			m_footprint = Blueprint.Footprint.Rotated((int)Orientation);
+		}
+
+		#endregion
+
+		//#################### PUBLIC METHODS ####################
+		#region
+
+		/// <summary>
+		/// Updates the component based on elapsed time and user input.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		public override void Update(GameTime gameTime)
+		{
+			switch(State)
+			{
+				case PlacementComponentState.IN_CONSTRUCTION:
+				{
+					ConstructionDone += gameTime.ElapsedGameTime.Milliseconds;
+					if(ConstructionDone == Blueprint.TimeToConstruct)
+					{
+						State = PlacementComponentState.OPERATING;
+					}
+					break;
+				}
+				case PlacementComponentState.IN_DESTRUCTION:
+				{
+					ConstructionDone -= gameTime.ElapsedGameTime.Milliseconds;
+					if(ConstructionDone == 0)
+					{
+						// TODO
+						//EntityDestructionManager.QueueForDestruction(Entity);
+					}
+					break;
+				}
+				default:	// PlacementComponentState.OPERATING
+				{
+					break;
+				}
+			}
 		}
 
 		#endregion
